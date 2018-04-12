61d15156e9a18e7a1220fd9c95cf50890a030a3c
Marjan Pečiukonis <MPE@tiatechnology.com>
2018-04-12 21:59:18 +0300

    Paskaita - JQuerry ivadas

---
+++ JS/1_ivadas/js/library.js
@@ -0,0 +1,10 @@
+function prekesKitaPuse(masyvas){
+  masyvas.reverse();
+  for (var i = 0; i < masyvas.length; i++) {
+    console.log(prekes[i]);
+  }
+}
+
+function Add(a, b) {
+  console.log(a+b);
+}
