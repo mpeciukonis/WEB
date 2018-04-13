5ab319eba451e23089af03003551b17a83e5601e
Marjan Pečiukonis <MPE@tiatechnology.com>
2018-04-13 21:36:28 +0300

    Paskaita - jquery animation, dynamic element creation

---
+++ JS/2_jquerry/reiksmes/js/calculator.js
@@ -0,0 +1,15 @@
+function Add(a,b){
+  return Number(a) + Number(b);
+}
+
+function Subtract(a,b){
+  return Number(a) - Number(b);
+}
+
+function Multiply(a,b){
+  return Number(a) * Number(b);
+}
+
+function Divide(a,b){
+  return Number(a) / Number(b);
+}
